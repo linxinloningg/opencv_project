b700a7aaa785a297b64074623bec4f615cc34f4a
linxinloningg <7377734+linxinloningg@user.noreply.gitee.com>
2022-01-15 13:50:14 +0800

    读取摄像头

---
+++ 1_3_read_cap.py
@@ -0,0 +1,15 @@
+import cv2
+
+# 读取摄像头
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    # 显示图片
+    if ret:
+        cv2.imshow('img', img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+# 释放内存
+cv2.destroyAllWindows()
